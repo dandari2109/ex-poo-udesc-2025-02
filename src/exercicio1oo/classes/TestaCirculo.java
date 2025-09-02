f3422818df9ec1b602d1c9094873f74550253e5a
dandari rossi <dandari.rossi@udesc.br>
2025-09-01 21:07:13 -0300

    Exercicio 3

---
+++ src/exercicio1oo/classes/TestaCirculo.java
@@ -0,0 +1,9 @@
+package exercicio1oo.classes;
+
+class TestaCirculo {
+    public static void main(String[] args) {
+        Circulo c1 = new Circulo();
+        c1.raio = 3.14;
+        System.out.println("raio: " + c1.raio);
+    }
+}
