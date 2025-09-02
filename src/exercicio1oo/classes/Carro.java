7182edf9ba857e17841d202711d56c953d5fe410
dandari rossi <dandari.rossi@udesc.br>
2025-09-01 21:25:18 -0300

    Exercicio 4

---
+++ src/exercicio1oo/classes/Carro.java
@@ -0,0 +1,8 @@
+package exercicio1oo.classes;
+
+class Carro {
+    String modelo;
+    String marca;
+    String ano;
+    String velocidade;
+}
---
+++ src/exercicio1oo/classes/TestaCarro.java
@@ -0,0 +1,15 @@
+package exercicio1oo.classes;
+
+class TestaCarro {
+    public static void main(String[] args) {
+        Carro c1 = new Carro();
+        c1.modelo = "Viper";
+        c1.marca = "Dodge";
+        c1.ano = "2009";
+        c1.velocidade = "365 Km";
+        System.out.println("Modelo: " + c1.modelo);
+        System.out.println("Marca: " + c1.marca);
+        System.out.println("Ano: " + c1.ano);
+        System.out.println("Velocidade: " + c1.velocidade);
+    }
+}
